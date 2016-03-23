b16a49f89dbcd6a8aecec8a044df80efaae06887
JoaoColaco94 <joaocolaco94@gmail.com>
2016-03-23 16:54:03 +0000

    Fixed some stuff. ASSERTS added

--- result.c
+++ result.c
@@ -1,4 +1,5 @@
 #include "result.h"
+#include "assert.h"
 
 struct result new_stack_result(void){
 
@@ -10,37 +11,68 @@ struct result new_stack_result(void){
 }
 
 uint8_t result_get_maxsat_value (struct result *result){
-  //#pragma omp critical
+  ASSERT_NON_NULL(result);
+
   return result->maxsat_value;
+
+  on_error:
+  ASSERT_EXIT();
 }
 
-void result_set_maxsat_value (struct result *result, uint8_t new_maxsat_value){
-  //#pragma omp critical
+void result_set_maxsat_value (struct result *result, uint16_t new_maxsat_value){
+  ASSERT_NON_NULL(result);
+
   result->maxsat_value = new_maxsat_value;
+
+  return;
+
+  on_error:
+  ASSERT_EXIT();
 }
 
 uint64_t result_get_na (struct result *result){
-  //#pragma omp critical
+  ASSERT_NON_NULL(result);
+
   return result->na;
+
+  on_error:
+  ASSERT_EXIT();
 }
 
 void result_set_na (struct result *result, uint64_t number_assignments){
-  //#pragma omp critical
+  ASSERT_NON_NULL(result);
+
   result->na = number_assignments;
+
+  return;
+
+  on_error:
+  ASSERT_EXIT();
 }
 
 struct assignment result_get_assignment_sample(struct result *result){
-  //#pragma omp critical
+  ASSERT_NON_NULL(result);
+
   return result->sample;
+
+  on_error:
+  ASSERT_EXIT();
 }
 
 void result_set_assignment_sample(struct result *result, struct assignment new_sample){
-  //#pragma omp critical{}
-  result->sample.vars[0] = new_sample.vars[0];
-  result->sample.vars[1] = new_sample.vars[1];
+  ASSERT_NON_NULL(result);
+
+  result->sample = new_sample;
+
+  return;
+
+  on_error:
+  ASSERT_EXIT();
 }
 
-bool result_update(struct result *result, uint8_t new_maxsat_value, struct assignment new_sample){
+bool result_update(struct result *result, uint16_t new_maxsat_value, struct assignment new_sample){
+  ASSERT_NON_NULL(result);
+
   if (new_maxsat_value == result->maxsat_value){
     //#pragma omp atomic FIXME
     (result->na)++;
@@ -48,12 +80,15 @@ bool result_update(struct result *result, uint8_t new_maxsat_value, struct assig
     //#pragma omp critical FIXME
     {
       result->maxsat_value = new_maxsat_value;
-      result->sample.vars[0] = new_sample.vars[0];
-      result->sample.vars[1] = new_sample.vars[1];
+      result->sample = new_sample;
       result->na = 1;
     }
   } else {
     return false;
   }
+
   return true;
+
+  on_error:
+  ASSERT_EXIT();
 }
