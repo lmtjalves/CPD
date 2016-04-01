a7db6dc13564d3d010f325d9271db8820bc8b163
Bernardo Figueiredo <bernardopascoalfigueiredo@tecnico.ulisboa.pt>
2016-04-02 00:04:19 +0100

    Changed assert.h to used debug.h. Changed debug.h to not depend on NDEBUG. It now depend on a compile time flag DEBUG_LOG_LEVEL. If none is given it defaults to only showing errors

--- assert.h
+++ assert.h
@@ -7,58 +7,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* Repeating macro from debug.h but without the variadic args because we're compiling with C89*/
-#define ASSERT_CLEAN_ERRNO() (errno == 0 ? "None" : strerror(errno))
-#define ASSERT_LOG_GENERIC(LOG_TYPE, MESSAGE) \
-do {\
-fprintf(stderr, LOG_TYPE " (%s:%d: errno: '%s' ) " MESSAGE "\n", __FILE__, __LINE__, ASSERT_CLEAN_ERRNO());\
-} while(0)
+#include "debug.h"
 
-#define ASSERT_LOG_GENERIC_VA(LOG_TYPE, MESSAGE, ...) \
-do {\
-fprintf(stderr, LOG_TYPE " (%s:%d: errno: '%s' ) " MESSAGE "\n", __FILE__, __LINE__, ASSERT_CLEAN_ERRNO(), __VA_ARGS__);\
-} while(0)
 
-#define ASSERT_LOG_BACKTRACE(MESSAGE) ASSERT_LOG_GENERIC("[BACKTRACE]", MESSAGE)
-#define ASSERT_LOG_BACKTRACE_VA(MESSAGE, ...) ASSERT_LOG_GENERIC_VA("[BACKTRACE]", MESSAGE, __VA_ARGS__)
-#define ASSERT_LOG_ASSERT(MESSAGE) ASSERT_LOG_GENERIC("[ASSERT]", MESSAGE)
-#define ASSERT_LOG_ASSERT_VA(MESSAGE, ...) ASSERT_LOG_GENERIC_VA("[ASSERT]", MESSAGE, __VA_ARGS__)
-
-#ifdef __linux__
-
-#include <execinfo.h>
-#define ASSERT_LOG_BACKTRACE_BACKTRACE() \
-do { \
-    /*using albb prefix to avoid shadowing other variables*/ \
-    const int albb_BACKTRACE_LIMIT = 100; \
-    void *albb_trace[albb_BACKTRACE_LIMIT]; \
-    char **albb_messages = (char **)NULL; \
-    int albb_i, albb_trace_size = 0; \
-\
-    albb_trace_size = backtrace(albb_trace, albb_BACKTRACE_LIMIT); \
-    albb_messages = backtrace_symbols(albb_trace, albb_trace_size); \
-    ASSERT_LOG_BACKTRACE("Execution_path:"); \
-    for (albb_i=0; albb_i < albb_trace_size; ++albb_i) { \
-        ASSERT_LOG_BACKTRACE_VA("%s", albb_messages[albb_i]); \
-    } \
-    free(albb_messages);\
-} while(0)
-
-#else
-
-#define ASSERT_LOG_BACKTRACE_BACKTRACE() ASSERT_LOG_BACKTRACE("Not printing backtrace because not running on linux!")
-
-#endif /*__linux__*/
-
-#define ASSERT_MSG(EXPR, MSG) ASSERT_MSG_VA(EXPR, MSG "%s", "")
 
 #define ASSERT_MSG_VA(EXPR, MSG, ...) do {\
     if (!(EXPR)) {\
-        ASSERT_LOG_ASSERT_VA("Evaluation of '" #EXPR "' failed. ||| "  MSG, __VA_ARGS__);\
-        ASSERT_LOG_BACKTRACE_BACKTRACE(); \
+        LOG_ERROR("Evaluation of '" #EXPR "' failed. ||| "  MSG, __VA_ARGS__);\
         goto on_error; /*Error handling*/\
     } \
 } while (0)
+
+#define ASSERT_MSG(EXPR, MSG) ASSERT_MSG_VA(EXPR, MSG "%s", "")
 
 #define ASSERT(EXPR) ASSERT_MSG(EXPR, "")
 
@@ -66,8 +26,7 @@ do { \
 
 #define ASSERT_EXIT() \
 do { \
-    ASSERT_LOG_ASSERT("Reached point of no return. Exiting."); \
-    ASSERT_LOG_BACKTRACE_BACKTRACE(); \
+    ASSERT_MSG(false, "Reached point of no return. Exiting."); \
     exit(1); \
 } while(0)
 
@@ -87,5 +46,11 @@ do { \
 ASSERT_NON_NULL(p); \
 free(p); \
 } while(0)
+
+#define ASSERT_ERROR(MSG) \
+do { \
+    ASSERT_MSG(false, MSG); \
+} while(0)
+
 
 #endif /*ASSERT_H__*/
--- debug.h
+++ debug.h
@@ -3,32 +3,46 @@
 #ifndef DEBUG_H__
 #define DEBUG_H__
 
-#ifdef NDEBUG
-
-#define LOG_GENERIC(LOG_TYPE, MESSAGE, ...)
-#define LOG_DEBUG(MESSAGE, ...)
-#define LOG_ERROR(MESSAGE, ...)
-#define LOG_WARN(MESSAGE, ...) 
-#define LOG_INFO(MESSAGE, ...) 
-#define SENTINEL(MESSAGE, ...) 
-
-#else
-
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 
-#define CLEAN_ERRNO() (errno == 0 ? "None" : strerror(errno))
+/*Implementation*/
 
-/* Public interface, so we only include these inside the ifdef NDEBUG*/
-#define LOG_GENERIC(LOG_TYPE, MESSAGE, ...) \
+#define DEBUG_LEVEL_ERROR 0
+#define DEBUG_LEVEL_WARN 10
+#define DEBUG_LEVEL_INFO 20
+#define DEBUG_LEVEL_DEBUG 30
+
+#ifndef DEBUG_LOG_LEVEL
+#define DEBUG_LOG_LEVEL DEBUG_LEVEL_ERROR
+#endif
+
+
+#define DEBUG_CLEAN_ERRNO() (errno == 0 ? "None" : strerror(errno))
+
+#define DEBUG_REAL_LOG_GENERIC(LOG_TYPE, MESSAGE, ...) \
 do {\
-fprintf(stderr, LOG_TYPE " (%s:%d: errno: %s ) " MESSAGE "\n", __FILE__, __LINE__, CLEAN_ERRNO(), __VA_ARGS__);\
+    fprintf(stderr, LOG_TYPE " (%s:%d: errno: %s) " MESSAGE "\n", __FILE__, __LINE__, DEBUG_CLEAN_ERRNO(), __VA_ARGS__);\
 } while(0)
-#define LOG_DEBUG(MESSAGE, ...) LOG_GENERIC("[DEBUG]", MESSAGE, __VA_ARGS__)
-#define LOG_ERROR(MESSAGE, ...) LOG_GENERIC("[ERROR]", MESSAGE, __VA_ARGS__)
-#define LOG_WARN(MESSAGE, ...) LOG_GENERIC("[WARN]", MESSAGE, __VA_ARGS__)
-#define LOG_INFO(MESSAGE, ...) LOG_GENERIC("[INFO]", MESSAGE, __VA_ARGS__)
-#endif /*NDEBUG*/
+
+
+/*Defined so we don't have to put "" at the end of every LOG*. It's used to allow for no arguments*/
+#define DEBUG_LOG_GENERIC(LOG_TYPE, LEVEL, MESSAGE, ...) \
+do { \
+    if ( LEVEL <= DEBUG_LOG_LEVEL) { \
+        DEBUG_REAL_LOG_GENERIC(LOG_TYPE, MESSAGE "%s", __VA_ARGS__, " "); \
+    } \
+} while(0)
+
+
+
+
+
+/* Public interface */
+#define LOG_ERROR(MESSAGE, ...) DEBUG_LOG_GENERIC("[ERROR]", DEBUG_LEVEL_ERROR, MESSAGE, __VA_ARGS__)
+#define LOG_WARN(MESSAGE, ...) DEBUG_LOG_GENERIC("[WARN]", DEBUG_LEVEL_WARN, MESSAGE, __VA_ARGS__)
+#define LOG_INFO(MESSAGE, ...) DEBUG_LOG_GENERIC("[INFO]", DEBUG_LEVEL_INFO, MESSAGE, __VA_ARGS__)
+#define LOG_DEBUG(MESSAGE, ...) DEBUG_LOG_GENERIC("[DEBUG]", DEBUG_LEVEL_DEBUG, MESSAGE, __VA_ARGS__)
 
 #endif /*DEBUG_H__*/
