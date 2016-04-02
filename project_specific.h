38902343f506a7052693d7315cc8f65ed437be31
Bernardo Figueiredo <bernardopascoalfigueiredo@tecnico.ulisboa.pt>
2016-04-02 11:01:33 +0100

    Fixed MAX_NUM_VARS in project_specific.h. Should be = to MAX_VAR

--- project_specific.h
+++ project_specific.h
@@ -1,8 +1,10 @@
 #include "omp.h"
 
 #define MAX_VAR 127
-#define MAX_NUM_VARS 128
+/* = MAX_VAR because we start at var 1*/
+#define MAX_NUM_VARS MAX_VAR
 #define MAX_NUM_CLAUSES 65536
 #define MAX_NUM_VARS_CLAUSE 20
+/* 4 because MAX_VAR is 127 and could be -127*/
 #define MAX_VAR_STR_SIZE 4
 #define NUM_INITIALIZED_VARS(num_vars) 8
