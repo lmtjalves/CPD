9bb2f00915025b4cecda68cbacb6ecc986fd54ce
lmtjalves@gmail.com <lmtjalves@gmail.com>
2016-05-07 12:15:13 +0100

    Updated script and added new tests

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -1,59 +1,60 @@
 import os, sys
 from time import sleep
-from subprocess import call
+from subprocess import call,Popen
 import re
 
 # File for the output
-thread_metrics = open('thread_metrics.csv', 'w+')
-maxsat_metrics = open('maxsat_metrics.csv', 'w+')
+thread_metrics = open('thread_metrics.csv', 'a')
+maxsat_metrics = open('maxsat_metrics.csv', 'a')
 
 
-def run_and_store_results(ex_name, filepath, num_slaves, num_threads_per_slave):
+def run_and_store_results(ex_name, filepath, num_slaves, num_threads_per_slave, num_iteration):
   # print filepath + str(num_slaves)
-  error_file = ex_name + "_" + str(num_slaves) + ".err"
-  out_file   = ex_name + "_" + str(num_slaves) + ".out"
-  log_file   = ex_name + "_" + str(num_slaves) + ".log"
+  error_file = ex_name + "_" + str(num_slaves) + "_" + str(num_iteration) + ".err"
+  out_file   = ex_name + "_" + str(num_slaves) + "_" + str(num_iteration) + ".out"
+  log_file   = ex_name + "_" + str(num_slaves) + "_" + str(num_iteration) + ".log"
+  
+  new_env = os.environ.copy()
+  new_env["OMP_NUM_THREADS"] = str(num_threads_per_slave)
 
-  # Set the amount of threads
-  os.environ['OMP_NUM_THREADS'] = num_threads_per_slave
+  if (not os.path.isfile(error_file) or os.stat(out_file).st_size == 0):
+    result = Popen([
+        "/mnt/cirrus/users/2/6/ist175926/condor_mpi",
+        "-n " + str(num_slaves + 1),
+        "-o " + out_file,
+        "-l " + log_file,
+        "-e " + error_file,
+        "/mnt/cirrus/users/2/6/ist175926/main",
+        "--debug",
+        filepath]).wait()
 
-  result = call([
-    "condor_mpi",
-    "-n " + str(num_slaves),
-    "-o " + out_file,
-    "-l " + log_file ,
-    "-e " + error_file,
-    "/mnt/cirrus/users/2/6/ist175926/main",
-    "--debug",
-    filepath])
+    # While the file is empty
+    while os.stat(out_file).st_size == 0:
+        sleep(1)
 
-  # While the file is empty
-  while os.stat(out_file).st_size == 0:
-    sleep(1)
+    # Get the result from the .err file
+    for line in open(error_file).readlines():
 
-  # Get the result from the .err file
-  for line in open(error_file).readlines():
+        # Get the metrics for the threads
+        if "total_computation_time" in line:
+            metrics = re.findall("\d+.\d+", line)
 
-    # Get the metrics for the threads
-    if "total_computation_time" in line:
-      metrics = re.findall("\d+.\d+", line)
+            # Append to the begin the amount of slaves and the filepath
+            metrics = [filepath, str(num_iteration), str(num_slaves), str(num_threads_per_slave)] + metrics
 
-      # Append to the begin the amount of slaves and the filepath
-      metrics = [filepath, str(num_slaves)] + metrics
+            # Write the metrics to the output file
+            thread_metrics.write(",".join(metrics) + "\n")
+            thread_metrics.flush()
 
-      # Write the metrics to the output file
-      thread_metrics.write(",".join(metrics) + "\n")
-      thread_metrics.flush()
+        # Maxsata total time from master
+        elif "maxsat time" in line:
+            metrics = re.findall("\d+.\d+", line)
+            # Append to the begin the amount of slaves and the filepath
+            metrics = [filepath, str(num_iteration), str(num_slaves), str(num_threads_per_slave)] + metrics
 
-    # Maxsata total time from master
-    elif "maxsat time" in line:
-      metrics = re.findall("\d+.\d+", line)
-      # Append to the begin the amount of slaves and the filepath
-      metrics = [filepath, str(num_slaves)] + metrics
-
-      # Write the metrics to the output file
-      maxsat_metrics.write(",".join(metrics) + "\n")
-      maxsat_metrics.flush()
+            # Write the metrics to the output file
+            maxsat_metrics.write(",".join(metrics) + "\n")
+            maxsat_metrics.flush()
 
 
 def main():
@@ -62,12 +63,9 @@ def main():
     if filename.endswith(".in"):
       filepath = folder + "/" + filename
 
-      for num_cores in [2, 4, 8, 16, 32, 64, 128, 256]:
-        if num_cores <= 4:
-          run_and_store_results(filename, filepath, 1, num_cores)
-        else:
-          # Simplification, all the others are multiples of 4
-          run_and_store_results(filename, filepath, num_cores/4, 4)
+      for num_cores in [1, 2, 4, 8, 16, 32, 64]:
+        for it in [1, 2, 3]:
+            run_and_store_results(filename, filepath, num_cores, 1, it)
 
 
 if __name__ == "__main__":
